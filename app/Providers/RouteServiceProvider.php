ad173f90f818af474828ef611f622f1ae79a7eec
Trọng Hiếu <hieuluucucxuc@gmail.com>
2025-04-08 03:08:15 +0700

    Update RouteServiceProvider.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -10,9 +10,14 @@ class RouteServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->routes(function () {
+            // API route
             Route::middleware(['api', 'json'])
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
+
+            // Web route cho /
+            Route::middleware('web')
+                ->group(base_path('routes/web.php'));
         });
     }
-} 
+}
